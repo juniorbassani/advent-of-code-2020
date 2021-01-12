25043dacb67dc04fd118e39f034f1ac5794cf919
Júnior Bassani <junior.eduardo.bassani@gmail.com>
2021-01-12 20:13:59 -0300

    Day 12 part two

--- src/day12.rs
+++ src/day12.rs
@@ -32,7 +32,7 @@ impl Add<i32> for Direction {
             90 => directions[(curr_direction + 1) % directions.len()],
             180 => directions[(curr_direction + 2) % directions.len()],
             270 => directions[(curr_direction + 3) % directions.len()],
-            // Return the same direction otherwise
+            // Maintain the same direction otherwise
             _ => self,
         }
     }
@@ -48,7 +48,7 @@ struct Coordinates {
 }
 
 impl Coordinates {
-    fn turn(&mut self, left_right: char, mut degrees: i32) {
+    fn rotate(&mut self, left_right: char, mut degrees: i32) {
         if left_right == 'L' {
             degrees = -degrees;
         }
@@ -65,7 +65,65 @@ impl Coordinates {
     }
 }
 
-fn manhattan_distance(input: &[&str]) -> u32 {
+#[derive(Copy, Clone, Debug)]
+struct Waypoint {
+    x: i32,
+    y: i32
+}
+
+impl Waypoint {
+    fn forward(&self, ship: &mut Coordinates, amount: i32) {
+        ship.north += if self.y > 0 { amount * self.y } else { 0 };
+        ship.south += if self.y < 0 { amount * -self.y } else { 0 };
+        ship.east += if self.x > 0 { amount * self.x } else { 0 };
+        ship.west += if self.x < 0 { amount * -self.x } else { 0 };
+    }
+
+    fn rotate(&mut self, direction: char, mut degrees: i32) {
+        let old_x = self.x;
+        let old_y = self.y;
+
+        if direction == 'L' {
+            degrees = -degrees;
+        }
+        
+        match degrees {
+            // Left
+            -90 => {
+                self.x = old_y * -1;
+                self.y = old_x;
+            },
+            -180 => {
+                self.x = old_x * -1;
+                self.y = old_y * -1;
+            },
+            -270 => {
+                self.x = old_y;
+                self.y = old_x * -1;
+            },
+            // Right
+            90 => {
+                self.x = old_y;
+                self.y = old_x * -1;
+            },
+            180 => {
+                self.x = old_x * -1;
+                self.y = old_y * -1;
+            },
+            270 => {
+                self.x = old_y * -1;
+                self.y = old_x;
+            },
+            // Maintain the same position otherwise
+            _ => {},
+        }
+    }
+}
+
+fn helper<F>(input: &[&str], mut f: F) -> u32
+where
+    F: FnMut(&mut Coordinates, char, i32),
+{
     let mut coordinates = Coordinates {
         north: 0,
         south: 0,
@@ -78,16 +136,7 @@ fn manhattan_distance(input: &[&str]) -> u32 {
         let direction = instruction.chars().nth(0).unwrap();
         let amount: i32 = instruction[1..].parse().unwrap_or_default();
 
-        match direction {
-            'N' => coordinates.north += amount,
-            'S' => coordinates.south += amount,
-            'E' => coordinates.east += amount,
-            'W' => coordinates.west += amount,
-            dir @ 'L' => coordinates.turn(dir, amount),
-            dir @ 'R' => coordinates.turn(dir, amount),
-            'F' => coordinates.forward(amount),
-            _ => panic!(),
-        }
+        f(&mut coordinates, direction, amount);
     }
 
     ((coordinates.north - coordinates.south).abs() + (coordinates.east - coordinates.west).abs())
@@ -95,10 +144,49 @@ fn manhattan_distance(input: &[&str]) -> u32 {
         .unwrap()
 }
 
+fn manhattan_distance(input: &[&str]) -> u32 {
+    helper(input, |coordinates, direction, amount| {
+        match direction {
+            'N' => coordinates.north += amount,
+            'S' => coordinates.south += amount,
+            'E' => coordinates.east += amount,
+            'W' => coordinates.west += amount,
+            'L' | 'R' => coordinates.rotate(direction, amount),
+            'F' => coordinates.forward(amount),
+            _ => panic!(),
+        }
+    })
+}
+
+fn waypoint_manhattan_distance(input: &[&str]) -> u32 {
+    let mut waypoint = Waypoint {
+        x: 10,
+        y: 1
+    };
+
+    helper(input, |coordinates, direction, amount| {
+        match direction {
+            'N' => waypoint.y += amount,
+            'S' => waypoint.y -= amount,
+            'E' => waypoint.x += amount,
+            'W' => waypoint.x -= amount,
+            'L' | 'R' => waypoint.rotate(direction, amount),
+            'F' => waypoint.forward(coordinates, amount),
+            _ => panic!(),
+        }
+    })
+}
+
 pub fn part1() -> u32 {
     let input = crate::get_input_as_vec::<String>(INPUT_PATH);
     let input: Vec<&str> = input.iter().map(|line| line.as_str()).collect();
     manhattan_distance(&input)
+}
+
+pub fn part2() -> u32 {
+    let input = crate::get_input_as_vec::<String>(INPUT_PATH);
+    let input: Vec<&str> = input.iter().map(|line| line.as_str()).collect();
+    waypoint_manhattan_distance(&input)
 }
 
 #[cfg(test)]
@@ -111,7 +199,12 @@ mod tests {
     }
 
     #[test]
-    fn test_example() {
+    fn compute_part2() {
+        assert_eq!(part2(), 89936);
+    }
+
+    #[test]
+    fn test_part1_example() {
         let mut example = Vec::new();
         example.push("F10");
         example.push("N3");
@@ -120,5 +213,17 @@ mod tests {
         example.push("F11");
 
         assert_eq!(manhattan_distance(&example), 25);
+    }
+
+    #[test]
+    fn test_part2_example() {
+        let mut example = Vec::new();
+        example.push("F10");
+        example.push("N3");
+        example.push("F7");
+        example.push("R90");
+        example.push("F11");
+
+        assert_eq!(waypoint_manhattan_distance(&example), 286);
     }
 }
