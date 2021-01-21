452b9fbb4f302dabecfafb8118d8199e0779edd5
Júnior Bassani <junior.eduardo.bassani@gmail.com>
2021-01-21 10:06:25 -0300

    Day 17 part two

--- src/day17.rs
+++ src/day17.rs
@@ -1,51 +1,77 @@
-use ndarray::Array3;
+use ndarray::{Array2, Array3, Array4};
 use std::collections::HashMap;
 
 const INPUT_PATH: &str = "input/day17";
 
-fn count_active_cells(pocket: &Array3<bool>) -> usize {
-    pocket.iter().filter(|&&cell| cell == true).count()
+fn count_active_cells<T>(pocket: T) -> usize
+where
+    T: IntoIterator,
+    T::Item: PartialEq<bool>,
+{
+    pocket.into_iter().filter(|cell| *cell == true).count()
 }
 
-fn active_neighbors(pocket: &Array3<bool>, z: usize, x: usize, y: usize) -> usize {
+fn count_active_neighbors<'a, T>(neighbors: T) -> usize
+where
+    T: Iterator<Item = Option<&'a bool>>,
+{
+    neighbors.filter(|n| matches!(n, Some(true))).count()
+}
+
+fn active_neighbors_3d(pocket: &Array3<bool>, x: usize, y: usize, z: usize) -> usize {
     let mut neighbors = Vec::with_capacity(26);
 
-    for k in z.checked_sub(1).unwrap_or_default()..=z + 1 {
-        for i in x.checked_sub(1).unwrap_or_default()..=x + 1 {
-            for j in y.checked_sub(1).unwrap_or_default()..=y + 1 {
+    for i in x.checked_sub(1).unwrap_or_default()..=x + 1 {
+        for j in y.checked_sub(1).unwrap_or_default()..=y + 1 {
+            for k in z.checked_sub(1).unwrap_or_default()..=z + 1 {
                 if k != z || i != x || j != y {
-                    neighbors.push(pocket.get((k, i, j)));
+                    neighbors.push(pocket.get((i, j, k)));
                 }
             }
         }
     }
 
-    neighbors
-        .iter()
-        .filter(|&n| matches!(n, Some(true)))
-        .count()
+    count_active_neighbors(neighbors.into_iter())
 }
 
-fn cubes_after_sixth_cycle(initial_state: &Array3<bool>) -> usize {
+fn active_neighbors_4d(pocket: &Array4<bool>, x: usize, y: usize, z: usize, w: usize) -> usize {
+    let mut neighbors = Vec::with_capacity(80);
+
+    for i in x.checked_sub(1).unwrap_or_default()..=x + 1 {
+        for j in y.checked_sub(1).unwrap_or_default()..=y + 1 {
+            for k in z.checked_sub(1).unwrap_or_default()..=z + 1 {
+                for h in w.checked_sub(1).unwrap_or_default()..=w + 1 {
+                    if k != z || i != x || j != y || h != w {
+                        neighbors.push(pocket.get((i, j, k, h)));
+                    }
+                }
+            }
+        }
+    }
+
+    count_active_neighbors(neighbors.into_iter())
+}
+
+fn cubes_after_sixth_cycle(initial_state: &Array2<bool>) -> usize {
     const ADDITIONAL: usize = 6 * 2;
     const DIMS: usize = ADDITIONAL + 1;
 
-    let lines = initial_state.shape()[1];
-    let columns = initial_state.shape()[2];
+    let lines = initial_state.shape()[0];
+    let columns = initial_state.shape()[1];
     let floor = f32::floor((DIMS / 2) as f32) as usize;
     let ceil = f32::ceil((DIMS / 2) as f32) as usize;
     let ceil = if floor == ceil { ceil + 1 } else { ceil };
 
-    let mut pocket = Array3::<bool>::default((DIMS, lines + ADDITIONAL, columns + ADDITIONAL));
+    let mut pocket = Array3::default((lines + ADDITIONAL, columns + ADDITIONAL, DIMS));
     let shape = pocket.shape();
-    let lines = shape[1];
-    let columns = shape[2];
+    let lines = shape[0];
+    let columns = shape[1];
 
     let mut init = initial_state.iter();
-    for z in floor..ceil {
-        for x in ADDITIONAL / 2..lines - ADDITIONAL / 2 {
-            for y in ADDITIONAL / 2..columns - ADDITIONAL / 2 {
-                pocket[[z, x, y]] = *init.next().unwrap();
+    for x in ADDITIONAL / 2..lines - ADDITIONAL / 2 {
+        for y in ADDITIONAL / 2..columns - ADDITIONAL / 2 {
+            for z in floor..ceil {
+                pocket[[x, y, z]] = *init.next().unwrap();
             }
         }
     }
@@ -53,57 +79,121 @@ fn cubes_after_sixth_cycle(initial_state: &Array3<bool>) -> usize {
     let mut changed = HashMap::with_capacity(64);
 
     for i in 1..=6 {
-        for z in floor.checked_sub(i).unwrap_or_default()..ceil + i {
-            for x in (ADDITIONAL / 2).checked_sub(i).unwrap_or_default()..lines - ADDITIONAL / 2 + i
+        for x in (ADDITIONAL / 2).checked_sub(i).unwrap_or_default()..lines - ADDITIONAL / 2 + i {
+            for y in
+                (ADDITIONAL / 2).checked_sub(i).unwrap_or_default()..columns - ADDITIONAL / 2 + i
             {
-                for y in (ADDITIONAL / 2).checked_sub(i).unwrap_or_default()
-                    ..columns - ADDITIONAL / 2 + i
-                {
-                    let neighbors = active_neighbors(&pocket, z, x, y);
+                for z in floor.checked_sub(i).unwrap_or_default()..ceil + i {
+                    let neighbors = active_neighbors_3d(&pocket, x, y, z);
 
-                    if pocket[[z, x, y]] {
+                    if pocket[[x, y, z]] {
                         if neighbors < 2 || neighbors > 3 {
-                            changed.insert((z, x, y), false);
+                            changed.insert((x, y, z), false);
                         }
                     } else {
                         if neighbors == 3 {
-                            changed.insert((z, x, y), true);
+                            changed.insert((x, y, z), true);
                         }
                     }
                 }
             }
         }
 
-        for (&(z, x, y), &v) in &changed {
-            pocket[[z, x, y]] = v;
+        for (&(x, y, z), &v) in &changed {
+            pocket[[x, y, z]] = v;
         }
         changed.clear();
     }
 
-    count_active_cells(&pocket)
+    count_active_cells(pocket.into_iter().copied())
 }
 
-pub fn part1() -> usize {
-    let mut input = Array3::<bool>::default((1, 8, 8));
+fn hypercubes_after_sixth_cycle(initial_state: &Array2<bool>) -> usize {
+    const ADDITIONAL: usize = 6 * 2;
+    const DIMS: usize = ADDITIONAL + 1;
+
+    let lines = initial_state.shape()[0];
+    let columns = initial_state.shape()[1];
+    let floor = f32::floor((DIMS / 2) as f32) as usize;
+    let ceil = f32::ceil((DIMS / 2) as f32) as usize;
+    let ceil = if floor == ceil { ceil + 1 } else { ceil };
+
+    let mut pocket = Array4::default((lines + ADDITIONAL, columns + ADDITIONAL, DIMS, DIMS));
+    let shape = pocket.shape();
+    let lines = shape[0];
+    let columns = shape[1];
+
+    let mut init = initial_state.iter();
+    for x in ADDITIONAL / 2..lines - ADDITIONAL / 2 {
+        for y in ADDITIONAL / 2..columns - ADDITIONAL / 2 {
+            for z in floor..ceil {
+                for w in floor..ceil {
+                    pocket[[x, y, z, w]] = *init.next().unwrap();
+                }
+            }
+        }
+    }
+
+    let mut changed = HashMap::with_capacity(128);
+
+    for i in 1..=6 {
+        for x in (ADDITIONAL / 2).checked_sub(i).unwrap_or_default()..lines - ADDITIONAL / 2 + i {
+            for y in
+                (ADDITIONAL / 2).checked_sub(i).unwrap_or_default()..columns - ADDITIONAL / 2 + i
+            {
+                for z in floor.checked_sub(i).unwrap_or_default()..ceil + i {
+                    for w in floor.checked_sub(i).unwrap_or_default()..ceil + i {
+                        let neighbors = active_neighbors_4d(&pocket, x, y, z, w);
+
+                        if pocket[[x, y, z, w]] {
+                            if neighbors < 2 || neighbors > 3 {
+                                changed.insert((x, y, z, w), false);
+                            }
+                        } else {
+                            if neighbors == 3 {
+                                changed.insert((x, y, z, w), true);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        for (&(x, y, z, w), &v) in &changed {
+            pocket[[x, y, z, w]] = v;
+        }
+        changed.clear();
+    }
+
+    count_active_cells(pocket.into_iter().copied())
+}
+
+fn parse_input() -> Array2<bool> {
+    let mut input = Array2::default((8, 8));
     let raw_input = crate::get_input_as_matrix(INPUT_PATH);
 
     for (x, line) in raw_input.iter().enumerate() {
         for (y, &cell) in line.iter().enumerate() {
-            input[[0, x, y]] = if cell == '#' { true } else { false };
+            input[[x, y]] = if cell == '#' { true } else { false };
         }
     }
 
+    input
+}
+
+pub fn part1() -> usize {
+    let input = parse_input();
     cubes_after_sixth_cycle(&input)
 }
 
 pub fn part2() -> usize {
-    0
+    let input = parse_input();
+    hypercubes_after_sixth_cycle(&input)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use ndarray::arr3;
 
     #[test]
     fn compute_part1() {
@@ -112,23 +202,28 @@ mod tests {
 
     #[test]
     fn compute_part2() {
-        println!("{}", part2());
-        // assert_eq!(part2(), 0);
+        assert_eq!(part2(), 2224);
     }
 
     #[test]
     fn part1_example() {
-        let initial_state = arr3(&[[
+        let initial_state = ndarray::arr2(&[
             [false, true, false],
             [false, false, true],
             [true, true, true],
-        ]]);
+        ]);
 
         assert_eq!(cubes_after_sixth_cycle(&initial_state), 112);
     }
 
     #[test]
     fn part2_example() {
-        assert_eq!(true, true);
+        let initial_state = ndarray::arr2(&[
+            [false, true, false],
+            [false, false, true],
+            [true, true, true],
+        ]);
+
+        assert_eq!(hypercubes_after_sixth_cycle(&initial_state), 848);
     }
 }
